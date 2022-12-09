0c39c0ac36d721655f61a890ac33a424b541614f
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-12-09 15:18:18 +0300

    areas

--- week1/areas.py
+++ week1/areas.py
@@ -1,2 +1,12 @@
 # Creating modules
 # To calculate areas
+import math
+# Triangle area
+def triangle(base, height):
+    return (base * height) / 2
+# Rectangle area
+def rectangle(base, height):
+    return base * height
+# Circle area
+def circle(radius):
+    return math.pi*(radius**2)
