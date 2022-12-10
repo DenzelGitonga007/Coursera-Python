ee654ae55af882b9ee2f5191729591ae77588db9
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-12-10 07:55:03 +0300

    using the 'with' method

--- week2/using_files.py
+++ week2/using_files.py
@@ -5,7 +5,15 @@
 # - Using the "with" method
 
 # Manual way
-my_file = open("week2\sample_file") # the current directory is week2, and then the file name is "sample_file"
+file_path = "week2\sample_file"
+my_file = open(file_path) # the current directory is week2, and then the file name is "sample_file"
 print(my_file.readline()) # readline() reads the contents on one line
 print("...")
 print(my_file.read()) # read() reads from the current position, to the end of the file
+# Close the file
+my_file.close()
+print("...")
+
+# Using the with method
+with open(file_path) as file: # I used file here, instead of the "my_file", but it's the same file
+    print(file.read())
