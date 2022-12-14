bd751f11385183e78c690309c87f6d5d1a6d95e7
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-12-14 20:10:41 +0300

    line by line

--- week2/iterating_files.py
+++ week2/iterating_files.py
@@ -1,1 +1,6 @@
 # Accessing the contents of a file, line by line
+with open("week2/sample_file") as text:
+    # Iterate now
+    for lines in text:
+        print(lines.upper()) # Print them in upper case
+        
