4e6112ba29be250156129a693bba986e30934dd6
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-02 11:12:48 +0300

    writing on a csv file

---
+++ using_python_to_interact_with_operating_system/week2/working_with_csv_files.py
@@ -0,0 +1,6 @@
+import csv
+hosts = [["Denzel", "0714082283"], ["Junne", "0726151007"]] # The values to input in the csv file
+# Open a csv file
+with open("hosts.csv", "w") as hosts_csv:
+    writer = csv.writer(hosts_csv)
+    writer.writerows(hosts)
