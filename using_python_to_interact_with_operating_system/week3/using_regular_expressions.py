b0aa72e975364d1774f4c8a198cf5d8b09982453
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-04 14:57:50 +0300

    using regex

--- using_python_to_interact_with_operating_system/week3/using_regular_expressions.py
+++ using_python_to_interact_with_operating_system/week3/using_regular_expressions.py
@@ -3,4 +3,8 @@ log_text = "July : 34839 computer dell inspiron [3583] working 24/7"
 # Get the index
 index = log_text.index("[")
 # Print the value at the index
-print(log_text[index+1:index+5])
+# print(log_text[index+1:index+5])
+
+# Using regex
+import re
+print(re.search(r"en", "Denzel", re.IGNORECASE))
