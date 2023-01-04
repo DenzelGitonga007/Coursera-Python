1dd656423673232df0359583f4bd056f15c117f5
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-04 11:05:48 +0300

    accessing strings without regex

---
+++ using_python_to_interact_with_operating_system/week3/using_regular_expressions.py
@@ -0,0 +1,6 @@
+# Accessing strings in a text without regex/regexp
+log_text = "July : 34839 computer dell inspiron [3583] working 24/7"
+# Get the index
+index = log_text.index("[")
+# Print the value at the index
+print(log_text[index+1:index+5])
