1d557e115dff544257deecba0413518621bd965f
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-05 15:34:37 +0300

    cases

--- using_python_to_interact_with_operating_system/week3/using _wildcards.py
+++ using_python_to_interact_with_operating_system/week3/using _wildcards.py
@@ -2,3 +2,4 @@
 import re
 print(re.search(r"a", "alpha"))
 print(re.search(r"[a-z].*[a-z]", "Denzel Python")) # Return lowercase letters from the 1st lowercase to the last
+print(re.search(r"[aA]", "Anaxothagoras")) # Cases
