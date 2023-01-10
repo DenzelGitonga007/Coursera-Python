a34715ac53b522b0311efac17ca0d4898e98cbb5
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-10 10:43:59 +0300

    return format using groups in capturing groups

--- using_python_to_interact_with_operating_system/week3/capturing_groups.py
+++ using_python_to_interact_with_operating_system/week3/capturing_groups.py
@@ -1,1 +1,15 @@
 # Return format
+# Returning first and last name
+import re
+# firstName = input("Enter your first name: ")
+# lastName = input("Enter your last name: ")
+# name = firstName + lastName
+def fullName(name):
+    result = re.search(r"^(\w*), (\w*)$", name)
+    print("The text under search is: "+ str(result.groups()))
+    # print(result[0]) -- prints the whole text
+    # print(result[1]) -- prints the first word
+    if result is None:
+        return name
+    return "I have nicknamed you: {} {}".format(result[2], result[1])
+print(fullName("Gitonga, Denzel"))
